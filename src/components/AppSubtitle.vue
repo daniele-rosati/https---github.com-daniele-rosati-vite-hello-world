8b2898905052280c6210f89b06b70849f4a397bc
daniele-rosati <drosati151@gmail.com>
2024-04-10 16:52:36 +0200

    img + subtitle

---
+++ src/components/AppSubtitle.vue
@@ -0,0 +1,14 @@
+<script>
+  // qui js
+  export default {
+    name: 'AppSubtitle',
+
+  }
+</script>
+
+<template>
+  <!-- qui html -->
+  <h3> sono il sottotitolo </h3>
+
+  <img src="/public/img/Screenshot 2024-03-22 134912.png" alt="">
+</template>
